976a6f29750c5c8297169958e2fadc038c2f09f9
dadibox <mmg.prokazyuk.o@gmail.com>
2020-11-25 08:42:40 +0200

    final commit

--- src/assets/hooks/useLocation.js
+++ src/assets/hooks/useLocation.js
@@ -1,35 +1,35 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const useLocation = () => {
-    const [location, setLocation] = useState({});
-    const [error, setError] = useState();
+  const [location, setLocation] = useState({});
+  const [error, setError] = useState();
 
-    const options = {enableHighAccuracy: false, timeout: 8000, maximumAge: 850000};
+  const options = {enableHighAccuracy: false, timeout: 8000, maximumAge: 850000};
 
-    const handleSuccess = (position) => {
-        const {latitude, longitude} = position.coords;
+  const handleSuccess = (position) => {
+    const { latitude, longitude } = position.coords;
 
-        setLocation({
-            latitude,
-            longitude,
-        });
-    };
+    setLocation({
+      latitude,
+      longitude,
+    });
+  };
 
-    const handleError = (error) => {
-        setError(error.message);
-    };
+  const handleError = (error) => {
+    setError(error.message);
+  };
 
-    useEffect(() => {
+  useEffect(() => {
 
-        if (!navigator.geolocation) {
-            setError("Geolocation is not supported.");
-            return;
-        }
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported.");
+      return;
+    }
 
-        navigator.geolocation.getCurrentPosition(handleSuccess, handleError, options);
-    }, []);
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError, options);
+  }, []);
 
-    return {location, error};
+  return { location, error };
 };
 
 export default useLocation;
--- src/components/Weather/LocalWeather.jsx
+++ src/components/Weather/LocalWeather.jsx
@@ -17,7 +17,9 @@ const LocalWeather = () => {
                 const apikey = `0f525f6e24d072a9dd5bbca6e92dc9b1`;
                 const lat = location.latitude;
                 const lngt = location.longitude;
+                // console.log(1)
 
+                //Works only with https certificate or add console.log(1) above to test the API
                 const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lngt}&appid=${apikey}&units=metric`)
                 dispatch(setWeatherData(response.data.main, response.data.name))
             } catch (err) {
