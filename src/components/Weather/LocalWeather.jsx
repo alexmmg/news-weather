82cbcdddfad6bc9b766545d084ee4b12206e09e0
alexmmg <42207466+alexmmg@users.noreply.github.com>
2020-11-25 12:15:50 +0200

    Update LocalWeather.jsx
    geolocation fix

--- src/components/Weather/LocalWeather.jsx
+++ src/components/Weather/LocalWeather.jsx
@@ -9,25 +9,14 @@ const LocalWeather = () => {
     const dispatch = useDispatch();
     const {location, error} = useLocation();
     const weather = useSelector(state => state.weather);
-
+    const apikey = `0f525f6e24d072a9dd5bbca6e92dc9b1`;
+    const lat = location.latitude;
+    const lngt = location.longitude;
 
     useEffect(() => {
-        const getWeather = async () => {
-            try {
-                const apikey = `0f525f6e24d072a9dd5bbca6e92dc9b1`;
-                const lat = location.latitude;
-                const lngt = location.longitude;
-                // console.log(1)
-
-                //Should be working with https certificate or change the position of the line "const response = ..." in code to see the position in development.
-                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lngt}&appid=${apikey}&units=metric`)
-                dispatch(setWeatherData(response.data.main, response.data.name))
-            } catch (err) {
-                console.log(err.message);
-            }
-        }
-        getWeather()
-    }, [])
+        const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lngt}&appid=${apikey}&units=metric`)
+                    .then (response => dispatch(setWeatherData(response.data.main, response.data.name)))
+    }, [location])
 
 
     return (
